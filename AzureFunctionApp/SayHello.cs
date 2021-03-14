04d21aee51181790f5316153e459e90574dfffb7
Pavel Shumihai <shumigaj@gmail.com>
2021-03-14 11:34:53 +0300

    AzureFunction project recreated. OpenApi disabled. ApplicationInsights configured in Azure for functions

--- AzureFunctionApp/SayHello.cs
+++ AzureFunctionApp/SayHello.cs
@@ -1,30 +1,17 @@
 using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace AzureFunctionApp
 {
-    /// <summary>
-    /// HttpTrigger Function available for Anonymous user
-    /// http://localhost:7074/api/SayHello?name=Shumigaj
-    /// https://azureeducationfunction.azurewebsites.net/api/SayHello?name=Pavel?name=Shumigaj
-    /// </summary>
     public static class SayHello
     {
         [FunctionName("SayHello")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
